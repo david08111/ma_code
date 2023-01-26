fixed augmentations area calculation for metrics to calculate properly TrainLogger caption creating from string list TrainLogger changed wandb section sorting Spherical_contrast_panoptic radius loss batch factor in normalization added Spherical_contrast_panoptic categorical loss item parts added Trainer Logging names adapted train_wandb_sweep.py WIP train_sweep_base.yaml added

data_handling/augmentations.py
@@ -74,7 +74,7 @@ class Augmentation_Wrapper():
 
         # plt.imshow(img)
         # plt.show()
-        # plt.imshow(mask)
+        # plt.imshow(mask[:, :, 0])
         # plt.show()
 
         # test = data_dict["annotations_data"]["image_id"].strip()
@@ -88,7 +88,30 @@ class Augmentation_Wrapper():
         transformed = self.transform(image=img, mask=mask)
 
         data_dict["img"] = transformed["image"]
-        data_dict["annotation_mask"] = transformed["mask"]
+        data_dict["annotation_mask"] = transformed["mask"]#.astype(np.uint32)
+
+        # plt.imshow(data_dict["annotation_mask"][:, :, 0])
+        # plt.show()
+
+        annotations_data_segments_dict_tmp = {el["id"]: el for el in data_dict["annotations_data"]["segments_info"]}
+        # annotations_data_segments_dict_tmp = {}
+        # for i in range(len(data_dict["annotations_data"]["segments_info"])):
+        #     el = data_dict["annotations_data"]["segments_info"][i]
+        #     annotations_data_segments_dict_tmp[el["id"]] = el
+        #     annotations_data_segments_dict_tmp[el["id"]]["indx"] = i
+            # calculate bbox and area for segments new
+        segment_ids, segment_id_areas = np.unique(data_dict["annotation_mask"][:, :, 0], return_counts=True)
+        for segment_id, segment_id_area in zip(segment_ids, segment_id_areas):
+            if segment_id == 0:
+                continue
+            if segment_id in annotations_data_segments_dict_tmp.keys():
+                annotations_data_segments_dict_tmp[segment_id]["area"] = segment_id_area
+                annotations_data_segments_dict_tmp[segment_id].pop("bbox")
+                # segment_id_indx = annotations_data_segments_dict_tmp[segment_id][indx]
+                # data_dict["annotations_data"]["segments_info"][segment_id_indx]["area"] = segment_id_area
+                # data_dict["annotations_data"]["segments_info"][segment_id_indx].pop("bbox")
+            else:
+                raise ValueError("Segment ID " + segment_id + " not in " + data_dict["annotations_data"]["image_id"])
 
         # plt.imshow(transformed["image"])
         # plt.show()

training/loss.py
@@ -28,7 +28,8 @@ class Loss_Wrapper():
             return Discriminative_contrast_loss(**loss_config)
         elif loss_type == "spherical_contrast_panoptic":
             return Panoptic_spherical_contrastive_loss(**loss_config)
-
+        elif loss_type == "spherical_contrast_panoptic_area_normed":
+            return Panoptic_spherical_contrastive_loss_area_normed(**loss_config)
         ## deprecated
         elif loss_type == "mse":
             return nn_modules.MSELoss(**loss_config)
@@ -171,6 +172,8 @@ class Panoptic_Quality(nn.Module):
         else:
             categories_dict = None
 
+        caption_name = logger.get_caption_from_name(name)
+
         for group_name in self.metric.keys():
             if "per_class" in group_name:
                 for cat_id in self.metric[group_name].keys():
@@ -188,9 +191,10 @@ class Panoptic_Quality(nn.Module):
                             metric_display_name = "Recognition Quality"
                         else:
                             continue
-                        caption = f"{name} - {metric_display_name} - {cat_id_name}"
+                        caption = f"{caption_name} - {metric_display_name} - {cat_id_name}"
                         logger.add_text(f"{caption} - {self.metric[group_name][cat_id][metric_name]}", logging.INFO, epoch)
-                        logger.add_scalar(f"{caption}", self.metric[group_name][cat_id][metric_name], epoch)
+                        caption_list = name + [metric_display_name, "Class", cat_id_name]
+                        logger.add_scalar(caption_list, self.metric[group_name][cat_id][metric_name], epoch)
             else:
                 for metric_name in self.metric[group_name].keys():
                     if metric_name == "pq":
@@ -201,9 +205,12 @@ class Panoptic_Quality(nn.Module):
                         metric_display_name = "Recognition Quality"
                     else:
                         continue
-                    caption = f"{name} - {metric_display_name} - {group_name}"
+                    caption = f"{caption_name} - {metric_display_name} - {group_name}"
                     logger.add_text(f"{caption} - {self.metric[group_name][metric_name]}", logging.INFO, epoch)
-                    logger.add_scalar(f"{caption}", self.metric[group_name][metric_name], epoch)
+                    # caption_list = name + [metric_display_name, group_name]
+                    # caption_list = name + [f"{metric_display_name} - {group_name}"]
+                    caption_list = name + [metric_display_name, group_name]
+                    logger.add_scalar(caption_list, self.metric[group_name][metric_name], epoch)
 
 
 
@@ -272,6 +279,8 @@ class Panoptic_spherical_contrastive_loss(nn.Module):
     def forward(self, outputs, masks, annotations_data):
         device = outputs.get_device()
 
+        loss_items_dict = {}
+
         # radius_loss = 0
         #
         # similarity_loss = 0
@@ -280,6 +289,9 @@ class Panoptic_spherical_contrastive_loss(nn.Module):
         similarity_loss = torch.tensor(0, dtype=torch.float32, device=device)
 
         unique_cat_ids = torch.unique(masks[:, 1, :, :])  # skip segment_id=0
+
+        # for unique_cat_id in unique_cat_ids[1:]:
+        #     loss_items_dict[f"Radius Loss - Cat ID {unique_cat_id}"] = 0
 
         outputs_reordered_tmp = torch.permute(outputs, (1, 0, 2, 3))
         masks_reordered_tmp = torch.permute(masks, (1, 0, 2, 3))
@@ -298,12 +310,25 @@ class Panoptic_spherical_contrastive_loss(nn.Module):
                 radius = self.hypsph_radius_map_list[cat_id_radius_indx]
                 outputs_indx_select = masks[:, 1, :, :] == unique_cat_id
                 outputs_cat_id_embeddings = outputs_reordered_tmp[:, outputs_indx_select]
+                # test = outputs_cat_id_embeddings[:, 0].detach().cpu().numpy()
+                # test2 = np.multiply(test, test.T)
+                # test3 = np.sum(test2)
                 outputs_cat_id_embeddings_norm = torch.norm(outputs_cat_id_embeddings, 2, dim=0)
                 # radius_sqared_loss_part = torch.full(outputs_cat_id_embeddings_norm.size(), radius*radius, device=device)
-                radius_sqared_loss_part = torch.full(outputs_cat_id_embeddings_norm.size(), radius, device=device, dtype=torch.float32)
-                loss_tmp = mse_loss_radius(outputs_cat_id_embeddings_norm, radius_sqared_loss_part)
+                radius_loss_part = torch.full(outputs_cat_id_embeddings_norm.size(), radius, device=device, dtype=torch.float32)
+                loss_tmp = mse_loss_radius(outputs_cat_id_embeddings_norm, radius_loss_part)
                 radius_loss += loss_tmp
+
+                loss_items_dict[f"Radius Loss/Part - Cat ID {unique_cat_id}"] = loss_tmp.item()
+                # loss_item_radius_key_tmp = f"Radius Loss - Cat ID {unique_cat_id}"
+                # if loss_item_radius_key_tmp not in loss_items_dict:
+                #     loss_items_dict[f"Radius Loss - Cat ID {unique_cat_id}"] = loss_tmp.item()
+                # else:
+                #     loss_items_dict[f"Radius Loss - Cat ID {unique_cat_id}"] += loss_tmp.item()
                 radius_loss_counter += 1
+
+        # test = outputs.shape[0]
+        radius_loss_counter *= outputs.shape[0] #take batch size into account for normalization
 
         ### instance discrimination part
 
@@ -342,6 +367,12 @@ class Panoptic_spherical_contrastive_loss(nn.Module):
                         similarity_loss += dot_product_embeddings_mean
                         similarity_loss_counter += 1
 
+                        loss_item_similarity_key_tmp = f"Similarity Loss/Part - Cat ID - {unique_cat_id} - Similar"
+                        if loss_item_similarity_key_tmp not in loss_items_dict:
+                            loss_items_dict[loss_item_similarity_key_tmp] = similarity_loss.item()
+                        else:
+                            loss_items_dict[loss_item_similarity_key_tmp] += similarity_loss.item()
+
 
                     for i in range(unique_segment_ids.shape[0] - 1):
                         unique_segment_id = unique_segment_ids[i]
@@ -365,15 +396,208 @@ class Panoptic_spherical_contrastive_loss(nn.Module):
                             similarity_loss += dot_product_embeddings_mean
                             similarity_loss_counter += 1
 
+                            loss_item_similarity_key_tmp = f"Similarity Loss/Part - Cat ID - {unique_cat_id} - Disimilar"
+                            if loss_item_similarity_key_tmp not in loss_items_dict:
+                                loss_items_dict[loss_item_similarity_key_tmp] = similarity_loss.item()
+                            else:
+                                loss_items_dict[loss_item_similarity_key_tmp] += similarity_loss.item()
+
+
         if radius_loss_counter > 0:
             radius_loss /= radius_loss_counter
+
+            for key in loss_items_dict.keys():
+                if "Radius" in key:
+                    loss_items_dict[key] /= radius_loss_counter
 
         if similarity_loss_counter > 0:
             similarity_loss /= similarity_loss_counter
 
+            for key in loss_items_dict.keys():
+                if "Similarity" in key:
+                    loss_items_dict[key] /= similarity_loss_counter
 
-        loss_items_dict = {"Radius Loss": radius_loss.item(),
-                           "Similarity Loss": similarity_loss.item()}
+
+
+        # loss_items_dict = {"Radius Loss": radius_loss.item(),
+        #                    "Similarity Loss": similarity_loss.item()}
+        loss_items_dict["Radius Loss"] = radius_loss.item()
+        loss_items_dict["Similarity Loss"] = similarity_loss.item()
+
+        total_loss = self.radius_loss_weight * radius_loss + self.similarity_loss_weight * similarity_loss
+
+        return total_loss, loss_items_dict
+
+class Panoptic_spherical_contrastive_loss_area_normed(nn.Module):
+    def __init__(self, cat_id_radius_order_map_list, radius_diff_dist=1, radius_start_val=0, cosine_emb_loss_margin=0, radius_loss_weight=0.5, similarity_loss_weight=0.5, hypsph_radius_map_list=None):
+        super().__init__()
+        self.cat_id_radius_order_map_list = cat_id_radius_order_map_list
+        self.radius_diff_dist = radius_diff_dist
+        self.radius_start_val = radius_start_val
+        self.cosine_emb_loss_margin = cosine_emb_loss_margin
+
+        self.mse_loss_radius = torch.nn.MSELoss()
+        # self.cosine_embedding_inst_discr_loss = torch.nn.CosineEmbeddingLoss(margin=cosine_emb_loss_margin, size_average=False, reduce=False, reduction=False)
+
+        self.radius_loss_weight = radius_loss_weight
+        self.similarity_loss_weight = similarity_loss_weight
+
+        if hypsph_radius_map_list:
+            self.hypsph_radius_map_list = hypsph_radius_map_list
+        else:
+            self.hypsph_radius_map_list = list(range(self.radius_start_val, self.radius_start_val + self.radius_diff_dist * len(self.cat_id_radius_order_map_list), self.radius_diff_dist))
+
+    def forward(self, outputs, masks, annotations_data):
+        device = outputs.get_device()
+
+        loss_items_dict = {}
+
+        # radius_loss = 0
+        #
+        # similarity_loss = 0
+        radius_loss = torch.tensor(0, dtype=torch.float32, device=device)
+
+        similarity_loss = torch.tensor(0, dtype=torch.float32, device=device)
+
+        unique_cat_ids = torch.unique(masks[:, 1, :, :])  # skip segment_id=0
+
+        # for unique_cat_id in unique_cat_ids[1:]:
+        #     loss_items_dict[f"Radius Loss - Cat ID {unique_cat_id}"] = 0
+
+        outputs_reordered_tmp = torch.permute(outputs, (1, 0, 2, 3))
+        masks_reordered_tmp = torch.permute(masks, (1, 0, 2, 3))
+
+        # mse_loss_radius = torch.nn.MSELoss(size_average=False, reduce=False, reduction=None)
+        mse_loss_radius = self.mse_loss_radius
+        # cosine_embedding_inst_discr_loss = self.cosine_embedding_inst_discr_loss
+
+        radius_loss_counter = 0
+        similarity_loss_counter = 0
+
+        if self.radius_loss_weight > float_precision_thr:
+            for unique_cat_id in unique_cat_ids[1:]:  # skip 0
+                unique_cat_id = int(unique_cat_id.item())
+                cat_id_radius_indx = self.cat_id_radius_order_map_list.index(unique_cat_id)
+                radius = self.hypsph_radius_map_list[cat_id_radius_indx]
+                outputs_indx_select = masks[:, 1, :, :] == unique_cat_id
+                outputs_cat_id_embeddings = outputs_reordered_tmp[:, outputs_indx_select]
+                area = outputs_cat_id_embeddings.shape[1]
+                # test = outputs_cat_id_embeddings[:, 0].detach().cpu().numpy()
+                # test2 = np.multiply(test, test.T)
+                # test3 = np.sum(test2)
+                outputs_cat_id_embeddings_norm = torch.norm(outputs_cat_id_embeddings, 2, dim=0)
+                # radius_sqared_loss_part = torch.full(outputs_cat_id_embeddings_norm.size(), radius*radius, device=device)
+                radius_loss_part = torch.full(outputs_cat_id_embeddings_norm.size(), radius, device=device, dtype=torch.float32)
+                loss_tmp = mse_loss_radius(outputs_cat_id_embeddings_norm, radius_loss_part)
+                loss_tmp /= area
+                radius_loss += loss_tmp
+
+                loss_items_dict[f"Radius Loss/Part - Cat ID {unique_cat_id}"] = loss_tmp.item()
+                # loss_item_radius_key_tmp = f"Radius Loss - Cat ID {unique_cat_id}"
+                # if loss_item_radius_key_tmp not in loss_items_dict:
+                #     loss_items_dict[f"Radius Loss - Cat ID {unique_cat_id}"] = loss_tmp.item()
+                # else:
+                #     loss_items_dict[f"Radius Loss - Cat ID {unique_cat_id}"] += loss_tmp.item()
+                radius_loss_counter += 1
+
+        # test = outputs.shape[0]
+        radius_loss_counter *= outputs.shape[0] #take batch size into account for normalization
+
+        ### instance discrimination part
+
+        # reduce amount of masks with "isthing" part from masks - B x H x W x (segment_id, cat_id, isthing)
+
+        batch_size = masks.shape[0]
+
+        if self.similarity_loss_weight > float_precision_thr:
+            for b in range(batch_size):
+
+                inst_discr_masks = masks_reordered_tmp[:2, b, masks[b, 2, :, :] == True]
+
+                unique_cat_ids = torch.unique(inst_discr_masks[1, :])
+
+                for unique_cat_id in unique_cat_ids:
+                    segments_id_data = masks_reordered_tmp[:, b, masks_reordered_tmp[1, b, :, :] == unique_cat_id]
+
+                    unique_segment_ids = torch.unique(segments_id_data[0, :])
+
+                    segment_id_embeddings_dict = {}
+                    # gather embeddings and calculate cosineembeddingloss with itself
+
+                    for unique_segment_id in unique_segment_ids:
+                        segment_id_embeddings = outputs[b, :, masks_reordered_tmp[0, b, :, :] == unique_segment_id]
+                        segment_id_embeddings_dict[unique_segment_id.item()] = segment_id_embeddings
+                        area = segment_id_embeddings.shape[1]
+
+                        segment_id_embeddings = torch.div(segment_id_embeddings, torch.norm(segment_id_embeddings, 2, dim=0) + 0.000001)
+                        dot_product_embeddings = torch.matmul(torch.transpose(segment_id_embeddings, 0, 1), segment_id_embeddings)
+
+                        dot_product_embeddings = torch.triu(dot_product_embeddings)
+                        # test = dot_product_embeddings.nonzero(as_tuple=True)
+                        dot_product_embeddings = dot_product_embeddings[dot_product_embeddings.nonzero(as_tuple=True)]
+                        dot_product_embeddings = torch.mul(dot_product_embeddings, -1)
+                        dot_product_embeddings = torch.add(dot_product_embeddings, 1)
+                        dot_product_embeddings_mean = torch.mean(dot_product_embeddings)
+                        dot_product_embeddings_mean /= area
+                        similarity_loss += dot_product_embeddings_mean
+                        similarity_loss_counter += 1
+
+                        loss_item_similarity_key_tmp = f"Similarity Loss/Part - Cat ID - {unique_cat_id} - Similar"
+                        if loss_item_similarity_key_tmp not in loss_items_dict:
+                            loss_items_dict[loss_item_similarity_key_tmp] = similarity_loss.item()
+                        else:
+                            loss_items_dict[loss_item_similarity_key_tmp] += similarity_loss.item()
+
+
+                    for i in range(unique_segment_ids.shape[0] - 1):
+                        unique_segment_id = unique_segment_ids[i]
+                        for neg_unique_segment_id in unique_segment_ids[i+1:]:
+
+                            curr_embedding = segment_id_embeddings_dict[unique_segment_id.item()]
+                            neg_embedding = segment_id_embeddings_dict[neg_unique_segment_id.item()]
+
+                            curr_embedding = torch.div(curr_embedding, torch.norm(curr_embedding, 2, dim=0) + 0.000001)
+                            neg_embedding = torch.div(neg_embedding, torch.norm(neg_embedding, 2, dim=0) + 0.000001)
+
+                            dot_product_embeddings = torch.matmul(torch.transpose(curr_embedding, 0, 1), neg_embedding)
+
+                            dot_product_embeddings = torch.triu(dot_product_embeddings)
+                            # test = dot_product_embeddings.nonzero(as_tuple=True)
+                            dot_product_embeddings = dot_product_embeddings[dot_product_embeddings.nonzero(as_tuple=True)]
+                            dot_product_embeddings = torch.add(dot_product_embeddings, -self.cosine_emb_loss_margin)
+                            dot_product_embeddings = torch.clamp(dot_product_embeddings, min=0)
+                            dot_product_embeddings_mean = torch.mean(dot_product_embeddings)
+
+                            similarity_loss += dot_product_embeddings_mean
+                            similarity_loss_counter += 1
+
+                            loss_item_similarity_key_tmp = f"Similarity Loss/Part - Cat ID - {unique_cat_id} - Disimilar"
+                            if loss_item_similarity_key_tmp not in loss_items_dict:
+                                loss_items_dict[loss_item_similarity_key_tmp] = similarity_loss.item()
+                            else:
+                                loss_items_dict[loss_item_similarity_key_tmp] += similarity_loss.item()
+
+
+        if radius_loss_counter > 0:
+            radius_loss /= radius_loss_counter
+
+            for key in loss_items_dict.keys():
+                if "Radius" in key:
+                    loss_items_dict[key] /= radius_loss_counter
+
+        if similarity_loss_counter > 0:
+            similarity_loss /= similarity_loss_counter
+
+            for key in loss_items_dict.keys():
+                if "Similarity" in key:
+                    loss_items_dict[key] /= similarity_loss_counter
+
+
+
+        # loss_items_dict = {"Radius Loss": radius_loss.item(),
+        #                    "Similarity Loss": similarity_loss.item()}
+        loss_items_dict["Radius Loss"] = radius_loss.item()
+        loss_items_dict["Similarity Loss"] = similarity_loss.item()
 
         total_loss = self.radius_loss_weight * radius_loss + self.similarity_loss_weight * similarity_loss
 

training/metrics_new.py
@@ -92,13 +92,45 @@ def pq_compute_single_core(annotation_set, output_img, mask_img, categories):
         # pan_pred = rgb2id(pan_pred)
         pan_pred = rgb2id(output_img[i, ...])
         pan_pred = pan_pred.astype(np.uint32)
+        # ####
+        # from matplotlib import pyplot as plt
+        # plt.imshow(pan_pred)
+        # plt.show()
+        # plt.imshow(pan_gt)
+        # plt.show()
+        # pred_mask_true = pan_pred == 8405120
+        # gt_mask_true = pan_gt == 8405120
+        # and_mask_true = np.bitwise_and(gt_mask_true, pred_mask_true)
+        #
+        # pred_vis = np.zeros(pan_pred.shape)
+        # pred_vis[pred_mask_true] = 1
+        #
+        # gt_vis = np.zeros(pan_pred.shape)
+        # gt_vis[gt_mask_true] = 1
+        #
+        # and_vis= np.zeros(pan_pred.shape)
+        # and_vis[and_mask_true] = 1
+        #
+        # plt.imshow(pred_vis)
+        # plt.show()
+        #
+        # plt.imshow(gt_vis)
+        # plt.show()
+        #
+        # plt.imshow(and_vis)
+        # plt.show()
+        # ####
+        #
+        # # for el in annotation_set[i][0]["segments_info"]:
+        # #     test = el["id"]
 
         gt_segms = {el['id']: el for el in annotation_set[i][0]['segments_info']}
         pred_segms = {el['id']: el for el in annotation_set[i][1]['segments_info']}
 
         # predicted segments area calculation + prediction sanity checks
         pred_labels_set = set(el['id'] for el in annotation_set[i][1]['segments_info'])
-        test = np.unique(pan_pred, return_counts=True)
+        # test = np.unique(pan_pred)
+        # test2 = np.unique(pan_gt)
         labels, labels_cnt = np.unique(pan_pred, return_counts=True)
         for label, label_cnt in zip(labels, labels_cnt):
             if label not in pred_segms:
@@ -126,6 +158,8 @@ def pq_compute_single_core(annotation_set, output_img, mask_img, categories):
         pred_matched = set()
         for label_tuple, intersection in gt_pred_map.items():
             gt_label, pred_label = label_tuple
+            # if gt_label == 8405120 and pred_label == 8405120:
+            #     print("TEST")
             if gt_label not in gt_segms:
                 continue
             if pred_label not in pred_segms:
@@ -135,6 +169,7 @@ def pq_compute_single_core(annotation_set, output_img, mask_img, categories):
             if gt_segms[gt_label]['category_id'] != pred_segms[pred_label]['category_id']:
                 continue
 
+            # test = gt_pred_map.get((VOID, pred_label), 0)
             union = pred_segms[pred_label]['area'] + gt_segms[gt_label]['area'] - intersection - gt_pred_map.get((VOID, pred_label), 0)
             iou = intersection / union
             if iou > 0.5:

utils/logger.py
@@ -5,6 +5,7 @@ import torch
 import numpy as np
 import wandb
 from utils.sampler import SamplerWrapper
+from copy import deepcopy
 
 from functools import reduce
 from torch.utils.tensorboard import SummaryWriter
@@ -14,6 +15,16 @@ def flatten_dict(dd, separator ='.', prefix =''):
              for kk, vv in dd.items()
              for k, v in flatten_dict(vv, separator, kk).items()
              } if isinstance(dd, dict) else { prefix : dd }
+
+def create_nested_dict_from_list(config_dict, name_list, value, epoch):
+    if len(name_list) > 1:
+        elem = name_list.pop(0)
+        config_dict[elem] = {}
+        create_nested_dict_from_list(config_dict[elem], name_list, value, epoch)
+    else:
+        elem = name_list.pop(0)
+        config_dict[elem] = value
+        config_dict["Epoch"] = epoch
 
 class TrainLogger(): # Wrapper for Logging to txt + TensorBoard + Wandb
 
@@ -25,7 +36,7 @@ class TrainLogger(): # Wrapper for Logging to txt + TensorBoard + Wandb
 
     graph_logged = False
 
-    def __init__(self, name, save_path, img_log_freq=10, num_log_img=20, embedd_log_freq=10, num_log_embedds=1, wandb_config=None, hyperparams_dict={}, embedding_max_sample_size=5000, sampler=None):
+    def __init__(self, name, save_path, img_log_freq=10, num_log_img=10, embedd_log_freq=10, num_log_embedds=1, wandb_config=None, hyperparams_dict={}, embedding_max_sample_size=5000, sampler=None):
         if not os.path.isdir(save_path):
             os.makedirs(save_path, exist_ok=True)
 
@@ -67,20 +78,94 @@ class TrainLogger(): # Wrapper for Logging to txt + TensorBoard + Wandb
         else:
             wandb.init(project="MA", entity="david08111", dir=wandb_path, config=hyperparams_dict)
 
+    def get_caption_from_name(self, name):
+        if isinstance(name, list):
+            caption_name = name[0]
+            for elem in name[1:]:
+                caption_name = f"{caption_name} - {elem}"
+            return caption_name
+        else:
+            return name
 
+    def get_wandb_section_from_name(self, name):
+        if isinstance(name, list):
+            caption_name = name[0]
+            for elem in name[1:]:
+                caption_name = f"{caption_name}/{elem}"
+            return caption_name
+        else:
+            return name
+
+    def get_log_msg_from_name_epoch_value(self, name, value, epoch):
+        if isinstance(name, list):
+            caption_name = name[0]
+            for elem in name[1:]:
+                caption_name = f"{caption_name} - {elem}"
+            log_msg = f"Epoch {epoch} - {caption_name} - {value}"
+            return log_msg
+        else:
+            return f"Epoch {epoch} - {name} - {value}"
+
+    def get_log_dict_from_name_epoch_value(self, name, value, epoch):
+        if isinstance(name, list):
+            wandb_log_dict = {}
+            name_list_tmp = list(name)
+            create_nested_dict_from_list(wandb_log_dict, name_list_tmp, value, epoch)
+            return wandb_log_dict
+        else:
+            {name: value,
+             "Epoch": epoch}
 
     def add_text(self, text, level, epoch):
+        text = self.get_caption_from_name(text)
+
         log_msg = f"Epoch {epoch} - {text}"
         logging.log(level, log_msg)
 
+    # def add_scalar_string_scalar(self, name, value, epoch):
+    #
+    #     log_msg = f"Epoch {epoch} - {name} - {value}"
+    #     logging.log(logging.INFO, log_msg)
+    #
+    #     self.tb_logger.add_scalar(name, value, epoch)
+    #
+    #     wandb.log({name: value,
+    #                "Epoch": epoch}, commit=True)
+    #
+    # def add_scalar_list_scalar(self, name_list, value, epoch):
+    #     caption_name = self.get_caption_from_name_list(name_list)
+    #     log_msg = f"Epoch {epoch} - {caption_name} - {value}"
+    #     logging.log(logging.INFO, log_msg)
+    #
+    #     self.tb_logger.add_scalar(caption_name, value, epoch)
+    #
+    #     wandb_log_dict = self.get_nested_dict_from_name_list_and_value(name_list, value, epoch)
+    #     # wandb.log({name: value,
+    #     #            "Epoch": epoch}, commit=True)
+    #     wandb.log(wandb_log_dict, commit=True)
+
     def add_scalar(self, name, value, epoch):
-        log_msg = f"Epoch {epoch} - {name} - {value}"
+        # if isinstance(name, str) and not isinstance(value, dict):
+        #     self.add_scalar_string_scalar(name, value, epoch)
+        #
+        # elif isinstance(name, list) and not isinstance(value, dict):
+        #     self.add_scalar_list_scalar(name, value, epoch)
+        # else:
+        #     raise NameError("Not implemented yet!")
+        log_msg = self.get_log_msg_from_name_epoch_value(name, value, epoch)
         logging.log(logging.INFO, log_msg)
 
-        self.tb_logger.add_scalar(name, value, epoch)
+        caption_name = self.get_caption_from_name(name)
+        self.tb_logger.add_scalar(caption_name, value, epoch)
 
-        wandb.log({name: value,
+        wandb_caption = self.get_wandb_section_from_name(name)
+
+        # wandb_log_dict = self.get_log_dict_from_name_epoch_value(name, value, epoch)
+        wandb.log({wandb_caption: value,
                    "Epoch": epoch}, commit=True)
+        # wandb.log(wandb_log_dict, commit=True)
+
+
 
 
     def add_image(self, name, img, annotations_data, epoch):
@@ -90,10 +175,21 @@ class TrainLogger(): # Wrapper for Logging to txt + TensorBoard + Wandb
 
         if epoch % self.img_log_freq == 0 and self.num_log_img_counter < self.num_log_img:
             for b in range(img.shape[0]):
-                log_msg = f"Epoch {epoch} - {name} - {annotations_data[b]['image_id']} - LOGGED"
+                name_tmp = deepcopy(name)
+                if isinstance(name, list):
+                    name_tmp.append(annotations_data[b]['image_id'])
+                else:
+                    name_tmp += " - " + annotations_data[b]['image_id']
+
+                caption = self.get_caption_from_name(name_tmp)
+
+                log_msg = f"Epoch {epoch} - {caption} - LOGGED"
                 logging.log(logging.DEBUG, log_msg)
 
-                caption = f"Epoch {epoch} - {name} - {annotations_data[b]['image_id']}"
+                # caption = f"Epoch {epoch} - {name} - {annotations_data[b]['image_id']}"
+
+
+                # caption = self.get_caption_from_name(name)
 
                 # self.tb_logger.add_images(caption, img[b], epoch, dataformats="NHWC")
                 self.tb_logger.add_images(caption, img[b], epoch, dataformats="HWC")
@@ -101,7 +197,10 @@ class TrainLogger(): # Wrapper for Logging to txt + TensorBoard + Wandb
                 # caption = f"Epoch {epoch} - {name} - {annotations_data['image_id']}"
                 wandb_img = wandb.Image(img[b], caption=caption)
 
-                wandb.log({name: wandb_img})
+                wandb_caption = self.get_wandb_section_from_name(name)
+
+                wandb.log({wandb_caption: wandb_img,
+                           "Epoch": epoch})
 
                 self.num_log_img_counter += 1
                 # self.tb_logger.add_image
@@ -134,12 +233,23 @@ class TrainLogger(): # Wrapper for Logging to txt + TensorBoard + Wandb
         if epoch % self.img_log_freq == 0 and self.num_log_img_and_mask_counter < self.num_log_img:
             for b in range(img.shape[0]):
             # for b in range(1):
-                log_msg = f"Epoch {epoch} - {name} - {annotations_data_gt[b]['image_id']} - LOGGED"
-                logging.log(logging.DEBUG, log_msg)
 
-                caption_img = f"Epoch {epoch} - {name} - IMG - {annotations_data_gt[b]['image_id']}"
-                caption_mask_gt = f"Epoch {epoch} - {name} - MASK GT - {annotations_data_gt[b]['image_id']}"
-                caption_mask_pred = f"Epoch {epoch} - {name} - MASK PRED - {annotations_data_gt[b]['image_id']}"
+                name_tmp = deepcopy(name)
+                if isinstance(name, list):
+                    name_tmp.append(annotations_data_gt[b]['image_id'])
+                else:
+                    name_tmp += " - " + annotations_data_gt[b]['image_id']
+
+                caption = self.get_caption_from_name(name_tmp)
+
+                log_msg_gt = f"Epoch {epoch} - {caption} - LOGGED"
+
+                # log_msg = f"Epoch {epoch} - {name} - {annotations_data_gt[b]['image_id']} - LOGGED"
+                logging.log(logging.DEBUG, log_msg_gt)
+
+                caption_img = f"{caption} - IMG"
+                caption_mask_gt = f"{caption} - MASK GT"
+                caption_mask_pred = f"{caption} - MASK PRED"
                 self.tb_logger.add_images(caption_img, img[b], epoch, dataformats="CHW")
                 self.tb_logger.add_images(caption_mask_gt, mask_gt[b], epoch, dataformats="CHW")
                 self.tb_logger.add_images(caption_mask_pred, mask_output[b], epoch, dataformats="CHW")
@@ -163,7 +273,10 @@ class TrainLogger(): # Wrapper for Logging to txt + TensorBoard + Wandb
                 segmentid_mask_gt_final = np.zeros(segmentid_mask_gt.shape, dtype=np.uint8)
                 segmentid_mask_pred_final = np.zeros(segmentid_mask_pred.shape, dtype=np.uint8)
 
-                caption = f"Epoch {epoch} - {name} - {annotations_data_gt[b]['image_id']}"
+                #######################
+
+
+                # caption = f"Epoch {epoch} - {name} - {annotations_data_gt[b]['image_id']}"
                 class_labels_gt = {}
                 class_labels_pred = {}
                 if categories_dict:
@@ -194,8 +307,9 @@ class TrainLogger(): # Wrapper for Logging to txt + TensorBoard + Wandb
                         if segment_uint8_counter > 255:
                             self.wandb_alert("Overflow",
                                              "Panoptic Segmentation mask calculation (for visualization) exceeds the class amount of 255! - visualization might be wrong")
-
-                wandb_img = wandb.Image(img[b], caption=caption, masks={
+                ##############
+                wandb_img_caption = f"Epoch {epoch} - {caption}"
+                wandb_img = wandb.Image(img[b], caption=wandb_img_caption, masks={
                     "ground_truth": {
                         "mask_data": segmentid_mask_gt_final,
                         "class_labels": class_labels_gt
@@ -206,7 +320,10 @@ class TrainLogger(): # Wrapper for Logging to txt + TensorBoard + Wandb
                     }
                 })
 
-                wandb.log({f"IMG {b}": wandb_img})
+                wandb_caption = self.get_wandb_section_from_name(name_tmp)
+
+                wandb.log({wandb_caption: wandb_img,
+                           "Epoch": epoch})
 
                 self.num_log_img_and_mask_counter += 1
 
@@ -231,6 +348,8 @@ class TrainLogger(): # Wrapper for Logging to txt + TensorBoard + Wandb
         for b in range(output_embeddings.shape[0]):
 
             if epoch % self.embedd_log_freq == 0 and self.num_log_embedds_counter < self.num_log_embedds:
+                name = self.get_caption_from_name(name)
+
                 if annotations_data:
                     caption_segment_ids = f"{name} - Segment Ids - {annotations_data[b]['image_id']}"
                     caption_cat_ids = f"{name} - Category Ids - {annotations_data[b]['image_id']}"
@@ -282,7 +401,8 @@ class TrainLogger(): # Wrapper for Logging to txt + TensorBoard + Wandb
                     columns = column_names
                 wandb_table = wandb.Table(columns=columns, data=final_embedding)
 
-                wandb.log({caption_segment_ids: wandb_table}, step=epoch)
+                wandb.log({f"Epoch {epoch} - {caption_segment_ids}": wandb_table,
+                           "Epoch": epoch})
 
                 self.num_log_embedds_counter += 1
 